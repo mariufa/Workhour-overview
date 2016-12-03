ac197682731ccc4098852f28af15b5070231cf0e
Marius Fagerland <mariusfagerland@yahoo.no>
2016-12-03 13:32:58 +0100

    Finish Timestamp_handler class. Not unit tested

--- timestamp_handler.py
+++ timestamp_handler.py
@@ -7,20 +7,47 @@ class Timestamp_handler(object):
         self.file_name = file_name
         self.timestamp = timestamp
         self.override_existing_today = override_existing_today
-        self.number_days_history = 80
+        self.NUMBER_DAYS_HISTORY = 80
 
     def register_timestamp(self):
-        opened_file = open(self.file_name)
+        timestamps = self.get_timestamps()
+        last_timestamp = self.get_last_timestamp(timestamps)
+
+
+
+
+        self.save_timestamps(timestamps)
+
+    def get_timestamps(self):
+        opened_file = open(self.file_name, 'r')
         timestamps = opened_file.readLines()
-        last_timestamp = float(timestamps[-1])
+        opened_file.close()
+        return timestamps
+
+    def get_last_timestamp(self, timestamps):
+        if (len(timestamps) == 0):
+            return -1
+        else:
+            return float(timestamps[-1])
+
+    def update_timestamps(self, timestamps)
         if (self.is_same_day(last_timestamp) and self.override_existing_today):
             timestamps[-1] = str(timestamps)
         else:
-            if ()
             timestamps.append(self.timestamp)
-        save_timestamps
+            if (len(timestamps) > self.NUMBER_DAYS_HISTORY):
+                timstamps.pop(0)
+        return timestamps
 
     def is_same_day(self, last_timestamp):
+        if (last_timestamp == -1):
+            return False
         current_date = datetime.fromtimestamp(self.timestamp).isocalendar()
         last_date = datetime.fromtimestamp(last_timestamp).isocalendar()
         return current_date == last_date
+
+    def save_timestamps(self, timestamps):
+        opened_file = open(self.file_name, 'w')
+        for stamp in timestamps:
+            opened_file.write("%s\n" % stamp)
+        opened_file.close()
