a659e77704450b63e97e2633fd0afd2e88697e55
Marius Fagerland <mariusfagerland@yahoo.no>
2016-12-04 20:25:35 +0100

    Finish test function test_register_timestamps

--- tests/test_timestamp_handler.py
+++ tests/test_timestamp_handler.py
@@ -12,13 +12,17 @@ class TestTimestampHandler(unittest.TestCase):
         test_file.close()
 
         now = time()
+        yesterday = now - 60*60*24
         self.handler = TimestampHandler(self.file_name, now, False)
+        self.handler.register_timestamp()
+        self.handler.timestamp = yesterday
+        self.handler.register_timestamp()
 
-    def test_get_empty_timestamps(self):
+    def test_register_timestamps(self):
         timestamps = self.handler.get_timestamps()
-        self.assertEqual([], timestamps)
+        self.assertEqual(2, len(timestamps))
 
-    def tearDown(self)
+    def tearDown(self):
         os.remove(self.file_name)
 
 if __name__ == '__main__':
